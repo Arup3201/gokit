80eed6b037f775a7735e616be22fb854d2e2f398
Arup3201 <arupjana7365@gmail.com>
2026-04-03 21:43:50 +0530

    feat: middleware for JWT login

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -2,8 +2,13 @@ package middlewares
 
 import (
 	"context"
+	"crypto/rsa"
 	"net/http"
 	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/Arup3201/gokit"
 )
 
 const (
@@ -23,17 +28,17 @@ type UserIdGetter interface {
 	GetUserIdFromSession(context.Context, string) (uint, error)
 }
 
-type authMiddleware struct {
+type sessionAuthenticator struct {
 	getter UserIdGetter
 }
 
-func NewAuthMiddleware(getter UserIdGetter) *authMiddleware {
-	return &authMiddleware{
+func NewSessionAuthenticator(getter UserIdGetter) *sessionAuthenticator {
+	return &sessionAuthenticator{
 		getter: getter,
 	}
 }
 
-func (a *authMiddleware) WithSession(next http.Handler) http.Handler {
+func (a *sessionAuthenticator) WithSession(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		var cookies = r.Cookies()
@@ -62,4 +67,48 @@ func (a *authMiddleware) WithSession(next http.Handler) http.Handler {
 		next.ServeHTTP(w, reqWithCtx)
 	})
 
+}
+
+type jwtAuthenticator struct {
+	publicKey *rsa.PublicKey
+}
+
+func NewJWTAuthenticator(publicKey *rsa.PublicKey) *jwtAuthenticator {
+	return &jwtAuthenticator{
+		publicKey: publicKey,
+	}
+}
+
+func (a *jwtAuthenticator) WithJWT(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bearer := r.Header.Get("Authorization")
+		if strings.Trim(bearer, " ") == "" {
+			// empty token
+			http.Error(w, "Authorization failure", http.StatusUnauthorized)
+			return
+		}
+
+		bearerToken := strings.Fields(bearer)
+		if bearerToken[0] != "Bearer" {
+			// malformed token
+			http.Error(w, "Authorization failure", http.StatusUnauthorized)
+			return
+		}
+
+		claims, err := gokit.ClaimsFromToken(bearerToken[1], a.publicKey)
+		if err != nil {
+			// invalid token signature or expired jwt or invalid jwt format
+			http.Error(w, "Authorization failure", http.StatusUnauthorized)
+			return
+		}
+
+		userId, err := strconv.Atoi(claims.UserId)
+		if err != nil {
+			// string to int parse failed
+			http.Error(w, "Authorization failure", http.StatusUnauthorized)
+			return
+		}
+		reqWithCtx := r.WithContext(NewContext(r.Context(), uint(userId)))
+		next.ServeHTTP(w, reqWithCtx)
+	})
 }
