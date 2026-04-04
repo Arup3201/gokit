32ac365b9d9539b3104b5a79be01cf65a7db9cd0
Arup3201 <arupjana7365@gmail.com>
2026-04-04 22:37:15 +0530

    update: added jwt based authentication example

---
+++ examples/jwt-auth-app/handlers/auth.go
@@ -0,0 +1,376 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"slices"
+	"strconv"
+	"time"
+
+	"github.com/Arup3201/gokit/auth"
+	"github.com/Arup3201/gokit/middlewares"
+)
+
+var (
+	ApiUrl               = "http://localhost:8080/api"
+	ResetPasswordPageUrl = "http://localhost:5173/reset-password"
+	RefreshTokenName     = "REFRESH_TOKEN"
+	TokenIssuer          = "JWTAuthenticator"
+)
+
+type Registerer interface {
+	Register(ctx context.Context,
+		email, fullName, password string) (uint, error)
+}
+
+type Emailer interface {
+	SendVerificationEmail(ctx context.Context,
+		serverUrl string,
+		userId, email, fullName string) error
+	VerifyEmail(ctx context.Context, token string) error
+}
+
+type PasswordResetter interface {
+	SendPasswordResetEmail(ctx context.Context,
+		destinationPageUrl string,
+		email string) error
+	ResetPassword(ctx context.Context,
+		token, password string) error
+}
+
+type Authenticator interface {
+	Login(ctx context.Context,
+		email, password string,
+		tokenIssuer string) (*auth.AccessAndRefreshToken, error)
+	Refresh(ctx context.Context,
+		token, tokenIssuer string) (*auth.AccessAndRefreshToken, error)
+	Logout(ctx context.Context,
+		sessionId string) error
+}
+
+type authController struct {
+	registerer       Registerer
+	emailer          Emailer
+	passwordResetter PasswordResetter
+	authenticator    Authenticator
+}
+
+func NewAuthController(registerer Registerer,
+	emailer Emailer,
+	resetter PasswordResetter,
+	auth Authenticator) *authController {
+	return &authController{
+		registerer:       registerer,
+		emailer:          emailer,
+		passwordResetter: resetter,
+		authenticator:    auth,
+	}
+}
+
+func (c *authController) Register(w http.ResponseWriter, r *http.Request) {
+
+	type registerData struct {
+		Email    string `json:"email"`
+		FullName string `json:"full_name"`
+		Password string `json:"password"`
+	}
+
+	var data registerData
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		log.Printf("[ERROR] json decode payload: %s\n", err)
+
+		http.Error(w,
+			"Error while parsing payload. Payload accepts email, full_name and password",
+			http.StatusBadRequest)
+		return
+	}
+
+	id, err := c.registerer.Register(r.Context(), data.Email, data.FullName, data.Password)
+	if err != nil {
+		log.Printf("[ERROR] authentication service register: %s\n", err)
+
+		http.Error(w,
+			"Registration failed. Make sure email is valid, full name and password is not empty",
+			http.StatusInternalServerError)
+		return
+	}
+
+	err = c.emailer.SendVerificationEmail(r.Context(),
+		ApiUrl,
+		strconv.Itoa(int(id)),
+		data.Email,
+		data.FullName)
+	if err != nil {
+		log.Printf("[ERROR] email service send verification email: %s\n", err)
+
+		http.Error(w,
+			"Failed to send verification email to the email",
+			http.StatusInternalServerError)
+		return
+	}
+
+	responseBody := map[string]any{
+		"id":      id,
+		"message": "User has registered successfully",
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(responseBody)
+}
+
+func (c *authController) VerifyEmail(w http.ResponseWriter, r *http.Request) {
+
+	token := r.URL.Query().Get("token")
+	if token == "" {
+		http.Error(w,
+			"Empty token not allowed",
+			http.StatusBadRequest)
+		return
+	}
+
+	err := c.emailer.VerifyEmail(r.Context(), token)
+	if err != nil {
+		log.Printf("[ERROR] verify email: %s\n", err)
+
+		http.Error(w,
+			"Failed to verify email",
+			http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "http://localhost:5173", http.StatusSeeOther)
+}
+
+func (c *authController) Login(w http.ResponseWriter, r *http.Request) {
+
+	type loginData struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var data loginData
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		log.Printf("[ERROR] json decode payload: %s\n", err)
+
+		http.Error(w,
+			"Error while parsing payload. Payload accepts email and password",
+			http.StatusBadRequest)
+		return
+	}
+
+	tokens, err := c.authenticator.Login(r.Context(),
+		data.Email, data.Password, TokenIssuer)
+	if err != nil {
+		log.Printf("[ERROR] auth service login: %s\n", err)
+
+		http.Error(w,
+			"Encountered error while logging in. Please try again with correct credentials.",
+			http.StatusInternalServerError)
+		return
+	}
+
+	refreshTokenCookie := &http.Cookie{
+		Name:     RefreshTokenName,
+		Value:    tokens.RefreshToken,
+		Expires:  tokens.RefreshTokenExpiresAt,
+		Path:     ApiUrl + "/auth/refresh",
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+	}
+	http.SetCookie(w, refreshTokenCookie)
+
+	responseBody := map[string]any{
+		"access_token": tokens.AccessToken,
+		"expires_at":   tokens.AccessTokenExpiresAt,
+	}
+	json.NewEncoder(w).Encode(responseBody)
+}
+
+func (c *authController) Refresh(w http.ResponseWriter, r *http.Request) {
+
+	var err error
+
+	cookies := r.Cookies()
+	tInd := slices.IndexFunc(cookies, func(c *http.Cookie) bool {
+		return c.Name == RefreshTokenName
+	})
+	if tInd == -1 {
+		log.Printf("[ERROR] Missing refresh token\n")
+
+		http.Error(w,
+			"User not authenticated",
+			http.StatusUnauthorized)
+		return
+	}
+
+	refreshToken := cookies[tInd].Value
+	if refreshToken == "" {
+		log.Printf("[ERROR] Empty refresh token\n")
+
+		http.Error(w,
+			"User not authenticated",
+			http.StatusUnauthorized)
+		return
+	}
+
+	tokens, err := c.authenticator.Refresh(r.Context(), refreshToken, TokenIssuer)
+	if err != nil {
+		log.Printf("[ERROR] auth service refresh: %s\n", err)
+
+		http.Error(w,
+			"Encountered internal error.",
+			http.StatusInternalServerError)
+		return
+	}
+
+	refreshTokenCookie := &http.Cookie{
+		Name:     RefreshTokenName,
+		Value:    tokens.RefreshToken,
+		Expires:  tokens.RefreshTokenExpiresAt,
+		Path:     ApiUrl + "/auth/refresh",
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+	}
+	http.SetCookie(w, refreshTokenCookie)
+
+	responseBody := map[string]any{
+		"access_token": tokens.AccessToken,
+		"expires_at":   tokens.AccessTokenExpiresAt,
+	}
+	json.NewEncoder(w).Encode(responseBody)
+}
+
+func (c *authController) PasswordResetEmail(w http.ResponseWriter, r *http.Request) {
+
+	type forgotPasswordReq struct {
+		Email string `json:"email"`
+	}
+
+	var data forgotPasswordReq
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		log.Printf("[ERROR] json decode payload: %s\n", err)
+
+		http.Error(w,
+			"Error while parsing payload. Payload accepts only email",
+			http.StatusBadRequest)
+		return
+	}
+
+	err := c.passwordResetter.SendPasswordResetEmail(r.Context(), ResetPasswordPageUrl, data.Email)
+	if err != nil {
+		log.Printf("[ERROR] auth service login: %s\n", err)
+
+		http.Error(w,
+			"Encountered error while sending password reset email. Please try again with valid email.",
+			http.StatusInternalServerError)
+		return
+	}
+
+	responseBody := map[string]any{
+		"message": "Reset password email has been sent to the email address",
+	}
+	json.NewEncoder(w).Encode(responseBody)
+}
+
+func (c *authController) ResetPassword(w http.ResponseWriter, r *http.Request) {
+
+	type resetPasswordReq struct {
+		Token    string `json:"token"`
+		Password string `json:"password"`
+	}
+
+	var data resetPasswordReq
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		log.Printf("[ERROR] json decode payload: %s\n", err)
+
+		http.Error(w,
+			"Error while parsing payload. Payload accepts token and password",
+			http.StatusBadRequest)
+		return
+	}
+
+	err := c.passwordResetter.ResetPassword(r.Context(), data.Token, data.Password)
+	if err != nil {
+		log.Printf("[ERROR] auth service reset password: %s\n", err)
+
+		http.Error(w,
+			"Encountered error while resetting password.",
+			http.StatusInternalServerError)
+		return
+	}
+
+	responseBody := map[string]any{
+		"message": "Password reset successful",
+	}
+	json.NewEncoder(w).Encode(responseBody)
+}
+
+func (c *authController) Logout(w http.ResponseWriter, r *http.Request) {
+
+	var err error
+
+	cookies := r.Cookies()
+	tInd := slices.IndexFunc(cookies, func(c *http.Cookie) bool {
+		return c.Name == RefreshTokenName
+	})
+	if tInd == -1 {
+		log.Printf("[ERROR] Missing refresh token\n")
+
+		http.Error(w,
+			"User not authenticated",
+			http.StatusUnauthorized)
+		return
+	}
+
+	refreshToken := cookies[tInd].Value
+	if refreshToken == "" {
+		log.Printf("[ERROR] Empty refresh token\n")
+
+		http.Error(w,
+			"User not authenticated",
+			http.StatusUnauthorized)
+		return
+	}
+	err = c.authenticator.Logout(r.Context(), refreshToken)
+	if err != nil {
+		log.Printf("[ERROR] auth service logout: %s\n", err)
+
+		http.Error(w,
+			"Encountered error while logout.",
+			http.StatusInternalServerError)
+		return
+	}
+
+	refreshTokenCookie := &http.Cookie{
+		Name:     RefreshTokenName,
+		Value:    "",
+		Expires:  time.Unix(0, 0),
+		Path:     ApiUrl + "/auth",
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+	}
+	http.SetCookie(w, refreshTokenCookie)
+
+	responseBody := map[string]any{
+		"message": "Logged out successfully",
+	}
+	json.NewEncoder(w).Encode(responseBody)
+}
+
+func (c *authController) Welcome(w http.ResponseWriter, r *http.Request) {
+
+	_, ok := middlewares.FromContext(r.Context())
+	if !ok {
+		http.Error(w, "Error extracting user data", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "Welcome to the site",
+	})
+}
---
+++ examples/jwt-auth-app/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Arup3201/gokit/auth"
+	"github.com/Arup3201/gokit/examples/jwt-auth-app/handlers"
+	"github.com/Arup3201/gokit/middlewares"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (
+	Host = "localhost"
+	Port = "8080"
+)
+
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func readRSAPrivateKey(filename string) (*rsa.PrivateKey, error) {
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("read file: %w", err)
+	}
+
+	block, _ := pem.Decode(bytes)
+	parseResult, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("x509 parse pkce#1 private key: %w", err)
+	}
+
+	privateKey := parseResult.(*rsa.PrivateKey)
+	return privateKey, nil
+}
+
+func main() {
+	var err error
+	var db *gorm.DB
+
+	db, err = gorm.Open(sqlite.Open("examples.db"), &gorm.Config{
+		TranslateError: true,
+	})
+	if err != nil {
+		log.Fatalf("gorm open failed with error: %s\n", err)
+	}
+
+	db.AutoMigrate(&auth.User{})
+	db.AutoMigrate(&auth.EmailVerification{})
+	db.AutoMigrate(&auth.PasswordResetToken{})
+	db.AutoMigrate(&auth.Session{})
+
+	RESEND_API_KEY := os.Getenv("RESEND_API_KEY")
+	if RESEND_API_KEY == "" {
+		log.Fatal("Missing RESEND_API_KEY\n")
+	}
+
+	privateKey, err := readRSAPrivateKey("private.key")
+	if err != nil {
+		log.Fatalf("rsa generate key: %s\n", err)
+	}
+
+	registerService := auth.NewRegisterService(db)
+	authService := auth.NewAuthWithJWT(db, privateKey)
+	passwordService := auth.NewPasswordService(RESEND_API_KEY, db)
+	emailService := auth.NewEmailService(RESEND_API_KEY, db)
+	middleware := middlewares.NewJWTAuthenticator(&privateKey.PublicKey)
+
+	controller := handlers.NewAuthController(registerService,
+		emailService,
+		passwordService,
+		authService)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /api/register", controller.Register)
+	mux.HandleFunc("GET /api/verify-email", controller.VerifyEmail)
+	mux.HandleFunc("POST /api/login", controller.Login)
+	mux.HandleFunc("POST /api/password-reset-link", controller.PasswordResetEmail)
+	mux.HandleFunc("POST /api/reset-password", controller.ResetPassword)
+
+	mux.Handle("POST /api/logout", middleware.WithJWT(http.HandlerFunc(controller.Logout)))
+	mux.Handle("GET /api/message", middleware.WithJWT(http.HandlerFunc(controller.Welcome)))
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf("%s:%s", Host, Port),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 20 * time.Second,
+		Handler:      CorsMiddleware(mux),
+	}
+
+	log.Printf("[INFO] Server starting at %s:%s\n", Host, Port)
+	log.Fatal(server.ListenAndServe())
+}
